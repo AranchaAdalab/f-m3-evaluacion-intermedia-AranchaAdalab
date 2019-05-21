c81291b8942c954ed46f4afb7b135728f7f2abdb
AranchaAdalab <aranzazunarvaez@gmail.com>
2019-05-21 23:25:47 +0200

    docs again

---
+++ docs/build/precache-manifest.8e45d7f0451d3d97f4cc6cd51e843de5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58183513c18b8ad6cfcabe595eb5566f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "670ce52befd0d43d6736",
+    "url": "./static/css/main.86ebb66e.chunk.css"
+  },
+  {
+    "revision": "db3927a79f0d16c235fc",
+    "url": "./static/js/2.13ef48b5.chunk.js"
+  },
+  {
+    "revision": "670ce52befd0d43d6736",
+    "url": "./static/js/main.85143213.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
